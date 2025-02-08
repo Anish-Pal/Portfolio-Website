935c429340ff7a4f96b52d477d357b10797815ce
Anish Pal <ap08anish@gmail.com>
2025-02-08 13:46:00 +0530

    Add SVG icons for phone, cross, menu, email, database, linkedin, code, backend, link, insta, github and implement tab functionality with menu toggle in script.js

---
+++ Assets/script.js
@@ -0,0 +1,54 @@
+let tabLinks = document.querySelectorAll(".tab-links");
+let tabContents = document.querySelectorAll(".tab-contents");
+
+tabLinks.forEach((tab, index) => {
+    tab.addEventListener("click", function () {
+        openTab(index);
+
+    });
+});
+function openTab(index) {
+    tabLinks.forEach(tab => 
+        tab.classList.remove("active-link"));
+    tabContents.forEach(content => 
+        content.classList.remove("active-tab"));
+
+    tabLinks[index].classList.add("active-link");
+    tabContents[index].classList.add("active-tab");
+}
+
+let cross = document.querySelector("#crossimg");
+let menu = document.querySelector("#menuimg");
+let getMenu = document.querySelector("#sidemenu");
+
+
+
+function closeMenu(){
+     getMenu.style.right = "-200px";
+}
+
+function openMenu(){
+   getMenu.style.right = "0";
+}
+
+cross.addEventListener("click",closeMenu);
+menu.addEventListener("click",openMenu);
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz3KeUjmLeKI2pjPOg8SqGj7o7FDUsKLFJHQhgxkMVarF5dVY0Gmhg-eoEZ9-6jbHuA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.querySelector("#msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        // msg.innerHTML = "Message sent successfully"
+        alert("Message sent successfully");
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000);
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
