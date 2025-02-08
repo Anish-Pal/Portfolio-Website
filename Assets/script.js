48e9a6dae43cb6766efd627930e26743e1ba041a
Anish Pal <ap08anish@gmail.com>
2025-02-08 15:19:01 +0530

    Add favicon and improve script formatting for menu functionality

--- Assets/script.js
+++ Assets/script.js
@@ -8,9 +8,9 @@ tabLinks.forEach((tab, index) => {
     });
 });
 function openTab(index) {
-    tabLinks.forEach(tab => 
+    tabLinks.forEach(tab =>
         tab.classList.remove("active-link"));
-    tabContents.forEach(content => 
+    tabContents.forEach(content =>
         content.classList.remove("active-tab"));
 
     tabLinks[index].classList.add("active-link");
@@ -21,18 +21,16 @@ let cross = document.querySelector("#crossimg");
 let menu = document.querySelector("#menuimg");
 let getMenu = document.querySelector("#sidemenu");
 
-
-
-function closeMenu(){
-     getMenu.style.right = "-200px";
+function closeMenu() {
+    getMenu.style.right = "-200px";
 }
 
-function openMenu(){
-   getMenu.style.right = "0";
+function openMenu() {
+    getMenu.style.right = "0";
 }
 
-cross.addEventListener("click",closeMenu);
-menu.addEventListener("click",openMenu);
+cross.addEventListener("click", closeMenu);
+menu.addEventListener("click", openMenu);
 
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz3KeUjmLeKI2pjPOg8SqGj7o7FDUsKLFJHQhgxkMVarF5dVY0Gmhg-eoEZ9-6jbHuA/exec'
@@ -40,15 +38,15 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.querySelector("#msg");
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response =>{
-        // msg.innerHTML = "Message sent successfully"
-        alert("Message sent successfully");
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000);
-        form.reset();
-    })
-    .catch(error => console.error('Error!', error.message))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            // msg.innerHTML = "Message sent successfully"
+            alert("Message sent successfully");
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
 })
